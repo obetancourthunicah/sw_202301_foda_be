24ba64e67812468d37180d791b285db98b304ad7
Orlando J Betancourth <orlando.betancourth@gmail.com>
2023-04-17 21:38:25 -0600

    Changes in Build.yml 2

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,5 +1,5 @@
 const { MongoMemoryServer } = require("mongodb-memory-server");
-process.env.MONGOMS_DOWNLOAD_URL=https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-6.0.5.tgz
+process.env.MONGOMS_DOWNLOAD_URL='https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-6.0.5.tgz';
 process.env.LOG_LEVEL = 'debug';
 process.env.MONGO_URI=process.env.MONGO_URL;
 process.env.MONGO_DB_NAME='jestDb';
