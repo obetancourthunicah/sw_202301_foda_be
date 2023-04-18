75f4ae862042ad4cc369748356e0163af90802db
Orlando J Betancourth <orlando.betancourth@gmail.com>
2023-04-18 16:15:46 -0600

    Adding Module Alias

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,4 +1,15 @@
 const { MongoMemoryServer } = require("mongodb-memory-server");
+const moduleAlias = require('module-alias');
+const srcPath = './src';
+moduleAlias.addAliases({
+  "@config": `${srcPath}/config`,
+  "@handlers": `${srcPath}/handlers`,
+  "@libs": `${srcPath}/libs`,
+  "@middleware": `${srcPath}/middleware`,
+  "@routes": `${srcPath}/routes`,
+  "@utils": `${srcPath}/utils`,
+  "@dao": `${srcPath}/dao`
+});
 process.env.MONGOMS_DOWNLOAD_URL='https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-6.0.5.tgz';
 process.env.LOG_LEVEL = 'debug';
 process.env.MONGO_URI=process.env.MONGO_URL;
