64d7d4540b15304b3bec0d0de6c86ab80652934a
Orlando J Betancourth <orlando.betancourth@gmail.com>
2023-04-18 16:09:49 -0600

    Configs in TSCondif

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import * as moduleAlias from 'module-alias';
 import logger from '@utils/logger';
-const srcPath = 'src';
+const srcPath = './src';
 moduleAlias.addAliases({
   "@config": `${srcPath}/config`,
-      "@handlers": `${srcPath}/handlers`,
-      "@libs": `${srcPath}/libs`,
-      "@middleware": `${srcPath}/middleware`,
-      "@routes": `${srcPath}/routes`,
-      "@utils": `${srcPath}/utils`,
-      "@dao": `${srcPath}/dao`
+  "@handlers": `${srcPath}/handlers`,
+  "@libs": `${srcPath}/libs`,
+  "@middleware": `${srcPath}/middleware`,
+  "@routes": `${srcPath}/routes`,
+  "@utils": `${srcPath}/utils`,
+  "@dao": `${srcPath}/dao`
 });
 
 import { createServer } from '@config/express';
@@ -21,13 +21,13 @@ const port = process.env.PORT || 3001;
 const startServer = () => {
   createServer().then((app) => {
     const server = http.createServer(app);
-    server.listen({host, port}, () => {
+    server.listen({ host, port }, () => {
       const address = server.address() as AddressInfo;
       logger.info(`Server is running on http://${address.address}:${address.port}`);
     });
     const signalTraps: NodeJS.Signals[] = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
     signalTraps.forEach((type) => {
-      process.once(type, async ()=> {
+      process.once(type, async () => {
         try {
           logger.info(`Process ${type} signal received`);
           logger.info('Closing http server');
@@ -46,8 +46,8 @@ const startServer = () => {
   });
 };
 
-MongoDBConn.getConnection().then(()=>{
+MongoDBConn.getConnection().then(() => {
   startServer();
-}).catch((error)=>{
+}).catch((error) => {
   console.error("No se pudo conectar a la DB", error);
 });
